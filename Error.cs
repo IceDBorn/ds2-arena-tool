b81676d5228e4785a02ec4b25653fee1242f2812
IceDBorn <icedborn@gmail.com>
2020-03-25 22:38:18 +0200

    Initial Commit

---
+++ Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DS2_Arena_Tool {
+    public partial class Error : Form {
+        public Error(bool flag) {
+            InitializeComponent();
+            label1.Text = flag.Equals(true) ? "You don't have a Dark Souls 2 Backup Folder!" : "You don't have a Dark Souls 2 Save Folder!";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
